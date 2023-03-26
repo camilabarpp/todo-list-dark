5a4b49f47c8d8f396dee37bcfca6105e29252780
Camila <camila@github.com>
2023-03-26 13:04:31 -0300

    Criei um botaão de criar tarefa para tela desktop

--- src/app/components/todo-routing.module.ts
+++ src/app/components/todo-routing.module.ts
@@ -1,12 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {TodoComponent} from "./todo.component";
+import {TodoFormComponent} from "./todo-form/todo-form.component";
 
 const routes = [
   {
     path: '',
     component: TodoComponent,
   },
+  // {
+  //   path: 'edit/:id',
+  //   component: TodoFormComponent,
+  // },
+  {
+    path: 'add',
+    component: TodoFormComponent,
+  }
 ];
 
 @NgModule({
