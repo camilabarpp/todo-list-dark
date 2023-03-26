eccaf5d3ad2fe50dea4e49dcc34b3d7bcc240adf
Camila <camila@github.com>
2023-03-26 11:35:24 -0300

    As listas estao dinamicas e funcionando perfeitamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  public showSideNav!: boolean;
-  currentTaskName: string = '';
-  currentTasks: any[] = []
-  monthlyTasks: any[] = [
-    {
-      id: 4,
-      taskTitle: 'Lunch with John',
-      completed: false,
-    },
-    {
-      id: 5,
-      taskTitle: 'Meditation at 6:00 AM',
-      completed: true,
-    }
-  ];
-  weeklyTasks: any[] = [
-    {
-      id: 2,
-      taskTitle: 'Pay for rent',
-      completed: false,
-    },
-    {
-      id: 3,
-      taskTitle: 'Check emails',
-      completed: true,
-    },
-  ];
-  dailyTasks: any[] = [
-    {
-      id: 1,
-      taskTitle: 'Daily meeting with team',
-      completed: true,
-    },
-    {
-      id: 6,
-      taskTitle: 'Buy fruits on supermarket',
-      completed: false
-    }
-  ];
+export class AppComponent {
 
-  toggleSideNav(): void {
-    this.showSideNav = !this.showSideNav;
-    if (this.showSideNav) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-  }
-
-  onMonthlyTasksSelected() {
-    this.currentTaskName = 'TAREFAS DO MÊS'
-    this.currentTasks = this.monthlyTasks;
-  }
-
-  onWeeklyTasksSelected() {
-    this.currentTaskName = 'TAREFAS DA SEMANA'
-    this.currentTasks = this.weeklyTasks;
-  }
-
-  onDailyTasksSelected() {
-    this.currentTaskName = 'TAREFAS DE HOJE'
-    this.currentTasks = this.dailyTasks;
-  }
-
-  onEdit($event: any) {
-    console.log('onEdit', $event)
-
-  }
-
-  onRemove($event: any) {
-    console.log('onRemove', $event)
-  }
-
-  onSelected($event: any) {
-    console.log('onSelected', $event)
-  }
-
-  ngOnInit(): void {
-    this.onDailyTasksSelected()
-  }
 }
