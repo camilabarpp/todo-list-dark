d63c2ad1d7ee6b754169dc8c2d772c2eb56231df
Camila <camila@github.com>
2023-03-26 10:29:41 -0300

    Criei os Outputs e os metodos de evento no sidenav component

--- src/app/components/todo-sidenav/todo-sidenav.component.ts
+++ src/app/components/todo-sidenav/todo-sidenav.component.ts
@@ -8,9 +8,24 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class TodoSidenavComponent {
   @Input() showSideNav!: boolean;
   @Output() toggledSideNav: EventEmitter<boolean> = new EventEmitter();
+  @Output() monthlyTasksSelected = new EventEmitter();
+  @Output() weeklyTasksSelected = new EventEmitter();
+  @Output() dailyTasksSelected = new EventEmitter();
 
   toggleSideNav(): void {
     this.toggledSideNav.emit();
     this.showSideNav = !this.showSideNav;
+  }
+
+  onMonthlyTasksClick() {
+    this.monthlyTasksSelected.emit();
+  }
+
+  onWeeklyTasksClick() {
+    this.weeklyTasksSelected.emit();
+  }
+
+  onDailyTasksClick() {
+    this.dailyTasksSelected.emit();
   }
 }
